190082dcf3363115f08a6598927ca5428afb4149
David Drummer <david.b.drummer@gmail.com>
2022-01-31 19:35:25 -0500

    commit tree 7df877370049135776d66975a548285aea049f4e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// create multiply variable
+let num1 = 2
+let num2 = 31
+let multiply = (num1 * num2);
 
-// Write your code here
+//create random variable
+let random = Math.round(Math.random() * 10) + 1;
+
+//create mod variable
+let num3 = 24;
+let num4 = 10;
+let mod = num3 % num4;
+
+//create variable max
+let max = Math.max(1, 2, 3, 4, 20);
